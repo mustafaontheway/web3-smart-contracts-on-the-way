62a0dc3c1594dcbabf545dca8987d056c2c5a192
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-09 20:10:23 +0300

    Update part-y-0003-constructor1.sol

--- part-y-0003-constructor1.sol
+++ part-y-0003-constructor1.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+contract Mustafa {
+
+    uint16 public expectedWeeklySales;
+
+    constructor(uint16 _s) {
+
+        expectedWeeklySales = _s;
+    }
+}
