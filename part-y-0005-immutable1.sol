1ff1241667d8ad1ccb1b941f16ecc6c6d7e39ce1
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-09 20:30:33 +0300

    Update part-y-0005-immutable1.sol

--- part-y-0005-immutable1.sol
+++ part-y-0005-immutable1.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+contract Mustafa {
+
+    uint16 public immutable FIXED_COST_USD;
+
+    constructor(uint16 _fc) {
+
+        FIXED_COST_USD = _fc;
+    }
+
+}
