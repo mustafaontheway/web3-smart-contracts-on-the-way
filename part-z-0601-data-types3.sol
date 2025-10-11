1e535a455708b242e550661523ca4726ed99ad19
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 23:33:24 +0300

    Create part-z-0601-data-types3.sol

---
+++ part-z-0601-data-types3.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract DataTypes {
+
+// Declare a variable of type int32 with the name 'myInt' and initialize it with the value -123
+
+    int32 myInt = -123;
+}
