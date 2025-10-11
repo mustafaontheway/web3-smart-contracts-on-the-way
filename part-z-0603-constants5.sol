a659c8d45cbf840c3b6747297347b73c4749d5e7
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-12 00:04:39 +0300

    Create part-z-0603-constants5.sol

---
+++ part-z-0603-constants5.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Constants {
+
+// Declare a constant of type uint256 with the name 'GAS_LIMIT' and initialize it with the value 3000000
+
+    uint256 constant GAS_LIMIT = 3000000;
+}
