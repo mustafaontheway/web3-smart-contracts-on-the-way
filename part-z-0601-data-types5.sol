3f41cb03ce6bb6f30678e3de80d863bb22044ecd
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 23:36:48 +0300

    Create part-z-0601-data-types5.sol

---
+++ part-z-0601-data-types5.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract DataTypes {
+
+// Declare a variable of type uint, int and address with the names 'myUint', 'myInt', and 'myAddress' respectively. Initialize 'myUint' with 123, 'myInt' with -123, and 'myAddress' with 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+
+    uint myUint = 123;
+    int myInt = -123;
+    address myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
