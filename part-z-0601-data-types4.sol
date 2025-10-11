2d219a4595b1a4b0a1e742ec6a4d0e62a733731a
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 23:34:43 +0300

    Create part-z-0601-data-types4.sol

---
+++ part-z-0601-data-types4.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract DataTypes {
+
+// Declare a variable of type address with the name 'myAddress' and initialize it with the value 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+
+    address myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
