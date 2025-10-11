0a8757fe5413685578282ee8183989b2a29606d0
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 23:31:21 +0300

    Create part-z-0601-data-types1.sol

---
+++ part-z-0601-data-types1.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract DataTypes {
+
+// Declare a variable of type bool with the name 'myBool' and initialize it with the value true
+
+    bool myBool = true;
+}
