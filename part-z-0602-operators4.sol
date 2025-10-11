42ff68a82752d4acea21816bc5c9b16a5a3da2c3
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 23:42:09 +0300

    Create part-z-0602-operators4.sol

---
+++ part-z-0602-operators4.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Operator {
+
+// Declare a variable of type uint256 with the name 'a'. Initialize it with 10. Then, use the left shift assignment operator to shift the bits of 'a' 2 places to the left
+
+    uint256 a = 10;
+
+    a <<= 2;
+}
