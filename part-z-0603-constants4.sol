492a2420672932ec7ce9f281748005838ecca26d
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 23:47:37 +0300

    Create part-z-0603-constants4.sol

---
+++ part-z-0603-constants4.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Constants {
+
+// Declare a constant of type string with the name 'CONTRACT_VERSION' and initialize it with the value 'v1.0.0'
+
+    string constant CONTRACT_VERSION = 'v1.0.0';
+}
