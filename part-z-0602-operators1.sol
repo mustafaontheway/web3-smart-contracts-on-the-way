2120762f65b7a925dfc9c92d7c110089e1265a4a
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 23:38:40 +0300

    Create part-z-0602-operators1.sol

---
+++ part-z-0602-operators1.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Operator {
+
+// Declare two variables of type uint256 with the names 'a' and 'b'. Initialize 'a' with 10 and 'b' with 2. Then, use the multiplication assignment operator to multiply 'a' by 'b'
+
+    uint256 a = 10;
+
+    uint256 b = 2;
+
+    a *= b;
+}
