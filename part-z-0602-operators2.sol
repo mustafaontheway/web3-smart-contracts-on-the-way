f5d46e91de4ff991ac992a4689f0f8522827476a
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 23:39:32 +0300

    Create part-z-0602-operators2.sol

---
+++ part-z-0602-operators2.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Operator {
+
+// Declare a variable of type uint256 with the name 'a'. Initialize it with 10. Then, use the increment operator to increase the value of 'a' by 1
+
+    uint256 a = 10;
+
+    a++;
+}
