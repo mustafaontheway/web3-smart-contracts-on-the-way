abf0e7497406b8f5e2b0eec0bcdae2ad7ff021b1
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-12 00:07:31 +0300

    Update part-z-0604-immutable1.sol

--- part-z-0604-immutable1.sol
+++ part-z-0604-immutable1.sol
@@ -1,1 +1,14 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
 
+contract Immutable {
+
+// Declare an immutable variable of type uint256 with the name 'MAX_BLOCKS' and assign it a value of 5000 in the constructor
+
+    uint256 immutable MAX_BLOCKS;
+
+    constructor() {
+
+        MAX_BLOCKS = 5000;
+    }
+}
