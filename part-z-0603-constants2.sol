4501f0f86c89558ee745593c183e95210f85c0f0
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 23:45:50 +0300

    Create part-z-0603-constants2.sol

---
+++ part-z-0603-constants2.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Constants {
+
+// Declare a constant of type string with the name 'TOKEN_NAME' and initialize it with the value 'MyToken'
+
+    string constant TOKEN_NAME = 'MyToken';
+}
