f989f2c05340def295855720708b72534b7991ec
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 23:44:50 +0300

    Create part-z-0603-constants1.sol

---
+++ part-z-0603-constants1.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Constants {
+
+// Declare a constant of type uint256 with the name 'MAX_BLOCKS' and initialize it with the value 5000
+
+    uint256 constant MAX_BLOCKS = 5000;
+}
