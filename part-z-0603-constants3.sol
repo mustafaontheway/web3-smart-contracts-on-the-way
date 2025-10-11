b317df4f10771f7c9f9d090d2079beca74463d9b
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 23:46:43 +0300

    Create part-z-0603-constants3.sol

---
+++ part-z-0603-constants3.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Constants {
+
+// Declare a constant of type string with the name 'TOKEN_TICKER' and initialize it with the value 'MTK'
+
+    string constant TOKEN_TICKER = 'MTK';
+}
