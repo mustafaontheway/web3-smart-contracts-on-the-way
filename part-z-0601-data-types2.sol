0b5f14e017d37402bee716b9d132b892f9e99923
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-11 23:32:40 +0300

    Create part-z-0601-data-types2.sol

---
+++ part-z-0601-data-types2.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract DataTypes {
+
+// Declare a variable of type uint256 with the name 'myUint' and initialize it with the value 123
+
+    uint256 myUint = 123;
+}
