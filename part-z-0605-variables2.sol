46ca426dc7059543d0837a9df367f1576485ff0f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-12 14:23:20 +0300

    Create part-z-0605-variables2.sol

---
+++ part-z-0605-variables2.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Variables {
+
+// Inside a function named 'calculate', declare a local variable of type uint256 with the name 'result' and initialize it with the value 500
+
+    function calculate() public {
+
+        uint256 result = 500;
+    }
+}
