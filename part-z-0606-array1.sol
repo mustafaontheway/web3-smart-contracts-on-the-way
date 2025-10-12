9adc8a80259027890648a8ace51b5e2a62d80430
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-12 17:48:38 +0300

    Create part-z-0606-array1.sol

---
+++ part-z-0606-array1.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Arrays {
+
+// Declare a fixed size array of type uint256 with the name 'fixedArray' and initialize it with the values 1, 2, 3
+
+    uint256[3] public fixedArray = [1, 2, 3];
+}
