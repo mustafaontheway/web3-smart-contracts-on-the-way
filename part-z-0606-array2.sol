39754f61f9ac543a67b68a3429f96e1a4ed60cd1
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-12 17:51:29 +0300

    Create part-z-0606-array2.sol

---
+++ part-z-0606-array2.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Arrays {
+
+// Declare a dynamic size array of type uint256 with the name 'dynamicArray'
+
+    uint256[] public dynamicArray;
+}
