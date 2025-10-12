b310b12d14f69ee738b95a587cd3da53c92bf53f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-12 19:03:12 +0300

    Create part-z-0607-mapping1.sol

---
+++ part-z-0607-mapping1.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Mapping {
+
+// Declare a mapping from address to uint256 with the name 'myMap'
+
+    mapping(address => uint256) public myMap;
+}
