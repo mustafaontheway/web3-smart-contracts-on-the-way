359bd7082e0ee091b8b006dd5dff21ba6bd4460f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-12 14:21:23 +0300

    Create part-z-0605-variables1.sol

---
+++ part-z-0605-variables1.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Variables {
+
+// Declare a state variable of type uint256 with the name 'totalSupply' and initialize it with the value 1000000
+
+    uint256 totalSupply = 1000000;
+}
