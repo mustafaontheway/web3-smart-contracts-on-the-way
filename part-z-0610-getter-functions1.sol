f47745b389a7564eb51ce4434f179dc6f682569d
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-14 10:07:12 +0300

    Create part-z-0610-getter-functions1.sol

---
+++ part-z-0610-getter-functions1.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract GetterFunctions {
+
+// Declare a public state variable of type uint named 'stateVar' and initialize it with the value 10
+
+    uint public stateVar = 10;
+}
