dc1e16b8ebfd7c8de6bb9b6a4862a80e57d5ddf5
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-14 10:45:28 +0300

    Create part-z-0611-enum1.sol

---
+++ part-z-0611-enum1.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Enumerables {
+
+// Declare an enum named 'State' with two states: 'Active' and 'Inactive'
+
+    enum State {
+
+        Active,
+        Inactive
+    }
+}
