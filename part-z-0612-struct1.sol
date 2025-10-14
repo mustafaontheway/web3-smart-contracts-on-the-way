5bb5741fc964f995dafca377ea3fcc0972bc723d
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-14 10:52:16 +0300

    Create  part-z-0612-struct1.sol

---
+++ part-z-0612-struct1.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Structs {
+
+// Declare a struct named 'Person' with two properties: 'name' of type string and 'age' of type uint
+
+    struct Person {
+
+        string name;
+        uint age;
+    }
+}
