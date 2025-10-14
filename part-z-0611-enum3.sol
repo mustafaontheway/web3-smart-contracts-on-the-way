ec48cf4cd74228f07e61a892ed03035473299bb0
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-14 10:48:56 +0300

    Create part-z-0611-enum3.sol

---
+++ part-z-0611-enum3.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract Enumerables {
+
+// Declare an enum named 'State' with two states: 'Active' and 'Inactive'
+// Declare a variable of type 'State' named 'currentState'
+// Initialize 'currentState' with 'State.Active'
+
+
+   enum State {
+
+        Active,
+        Inactive
+    }
+
+    State currentState = State.Active;
+
+}
