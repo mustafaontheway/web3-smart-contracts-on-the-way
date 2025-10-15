f7ba5d5d3ceee05c566b36291974dc4975f4b37e
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-15 22:34:16 +0300

    Create part-z-0613-conditional-statements1.sol

---
+++ part-z-0613-conditional-statements1.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract ConditionalStatements {
+
+// Declare a variable of type uint named 'number' and initialize it with the value 10
+// Use an 'if' statement to check if 'number' is greater than 5
+
+    uint number = 10;
+
+    if (number > 5) {
+        
+    }
+}
