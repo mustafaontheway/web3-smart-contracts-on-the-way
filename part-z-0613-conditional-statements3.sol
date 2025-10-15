82df5b112e2bde98cabdf8450f392cc6731baa06
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-15 22:37:02 +0300

    Add conditional statements to check number value
    Implement conditional logic to evaluate a number.

---
+++ part-z-0613-conditional-statements3.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.24;
+
+contract ConditionalStatements {
+
+// Declare a variable of type uint named 'number' and initialize it with the value 10
+// Use an 'if' statement to check if 'number' is greater than 5 and if true, declare a variable 'result' and set it to true
+// Add an 'else' statement that sets 'result' to false if 'number' is not greater than 5
+
+    uint number = 10;
+
+    bool result;
+
+    if (number > 5) {
+
+        result = true;
+    } else {
+
+        result = false;
+    }
+}
